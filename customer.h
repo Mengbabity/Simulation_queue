25749bf7fd07a4cc5423b0c8fd170c15430d3612
Han Meng <32949218+Mengbabity@users.noreply.github.com>
2018-06-14 13:44:00 +0800

    Create customer.h

---
+++ customer.h
@@ -0,0 +1,18 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include<queue>
+
+using namespace std;
+
+class customer
+{
+public:
+	int arrive_time;
+	int service_time;
+	int leave_time;
+	int number;
+	int service_number;
+};
+
+#endif
